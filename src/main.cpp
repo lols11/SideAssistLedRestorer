d40a49d48ce84c5ab19aeaa641d17dbb7ef7b917
lols11 <lols11@spoko.pl>
2023-08-16 05:41:24 +0200

    Update main.cpp
    - Added information about ongoing DPF filter regeneration.
    - Confirmed brightness mapping.
    - Removed the low power library along with its functions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,70 @@
 //Based on https://www.drive2.com/b/601314663967246021/
-//Modified by lols11
 #include <spi.h>
 #include <mcp2515.h>
-//#include "LowPower.h" // Low power, not required
+
 #define SAYellowPin 6
 #define SAGreenPin 9
 
-struct can_frame canMsg;
-static boolean celebration = true;
-static boolean manageGreenOnly = true;
+const static boolean manageLEDs = true;  //Should I manage the LED for side assist?
+const static boolean manageGreenOnly = true; //Should i've menage only green LED for SA?
+const static boolean celebration = true;     // Should the SA LED celebration be turned on?
+const static boolean showDPFPurge = true; //Should the information about DPF cleaning be displayed?
+const unsigned long blinkIntervalTime = 250;    //Interval for blinking DPF light when purging
+
+struct can_frame receivedCanMsg;
+struct can_frame infoCanMsg;
+struct can_frame drivetrainCanMsg;
+
+static boolean dpfPurgeActualState = false;
+static boolean dpfPurgeLastState = false;
+unsigned long blinkStartTime = 0;
+int brightness = 255;
+int canBrightness = 0x64; // Max value for instrument cluster brightness. 0x64 = 100(%) DEC
+
 MCP2515 mcp2515(10);
 
-canid_t SAid = 0x03BA; // Can ID for Side Assist
+canid_t infoID= 0x03BA;         // Can ID for controlling communicats in instrument cluster (Side assist, buzzer)
 canid_t brightnessID = 0x0470; // Can ID for brightness of instrument cluster
-int brightness = 255; 
-int canBrightness = 0x64; //Max value for instrument cluster brightness
+canid_t drivetrainID = 0x0480; // Can ID for drivetrain used for DPF purge info and turning DPF LED on
+
+
+static void sendBeep()
+{
+  infoCanMsg.data[2] = infoCanMsg.data[2] | 0b00000001;
+  mcp2515.sendMessage(&infoCanMsg);
+}
+
+static void showDPFPurgeInfo()
+{
+ 
+  if (!dpfPurgeActualState && !dpfPurgeLastState) //DPF Purge not active
+    return;
+
+  if (dpfPurgeActualState && !dpfPurgeLastState) //Start of DPF purge
+  {
+    blinkStartTime = millis();
+    sendBeep();
+    dpfPurgeLastState = true;
+  }
+
+  if (!dpfPurgeActualState && dpfPurgeLastState) //End of DPF purge
+  {
+    sendBeep();
+    dpfPurgeLastState = false;
+    return;
+  }
+
+  unsigned long actualTime = millis();
+  if (blinkStartTime + blinkIntervalTime >= actualTime) //DPF purge active (DPF LED blinking) 
+  {
+    drivetrainCanMsg.data[5] = drivetrainCanMsg.data[5] | 0b00000010;
+  mcp2515.sendMessage(&drivetrainCanMsg);
+  }
+  else if (blinkStartTime + blinkIntervalTime * 2 >= actualTime)
+  blinkStartTime= millis();
+
+  
+}
 
 void setup()
 {
@@ -36,50 +86,63 @@ void setup()
     {
       digitalWrite(SAYellowPin, HIGH);
       delay(500);
-      }
+    }
     digitalWrite(SAGreenPin, HIGH);
     delay(500);
     if (!manageGreenOnly)
     {
       digitalWrite(SAYellowPin, LOW);
       delay(250);
-      }
+    }
     digitalWrite(SAGreenPin, LOW);
   }
-      //============================
+
+}
+
+void loop()
+{
+
+  if (mcp2515.readMessage(&receivedCanMsg) == MCP2515::ERROR_OK)
+  {
+    if (showDPFPurge && receivedCanMsg.can_id == drivetrainID)
+    {
+      if ((receivedCanMsg.data[0] & 0b100001) == 0b100001){
+        dpfPurgeActualState=true;
+        drivetrainCanMsg=receivedCanMsg;
+    }else
+        dpfPurgeActualState=false;
+    
+    showDPFPurgeInfo();
     }
 
-    void loop()
+    if (manageLEDs && receivedCanMsg.can_id == brightnessID)
     {
-
-      if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)
+    canBrightness = receivedCanMsg.data[4];
+    brightness = map(canBrightness, 0x1B, 0x64, 69, 255);
+    return;
+    }
+    if (manageLEDs && receivedCanMsg.can_id == infoID)
+    {
+      if (receivedCanMsg.data[2] == 0x08 && !manageGreenOnly)
       {
-    if (canMsg.can_id == brightnessID)
-    {
-        canBrightness = canMsg.data[4];
-        brightness = map(canBrightness, 0x1B, 0x64, 69, 255);
-        return;
-        }
-
-        if (canMsg.can_id == SAid)
-        {
-        if (canMsg.data[2] == 0x08 && !manageGreenOnly)
-        {
-          analogWrite(SAYellowPin, brightness);
-          digitalWrite(SAGreenPin, LOW);
-          }
-          else if (canMsg.data[2] == 0x10)
-          {
-            digitalWrite(SAYellowPin, LOW);
-            analogWrite(SAGreenPin, brightness);
-          }
-          else
-          {
-            if (!manageGreenOnly)
-              digitalWrite(SAYellowPin, LOW);
-            digitalWrite(SAGreenPin, LOW);
-          }
-        }
+        analogWrite(SAYellowPin, brightness);
+        digitalWrite(SAGreenPin, LOW);
       }
-      //LowPower.powerDown(SLEEP_30MS, ADC_OFF, BOD_OFF); // Low power, not required
+      else if (receivedCanMsg.data[2] == 0x10)
+      {
+        digitalWrite(SAYellowPin, LOW);
+        analogWrite(SAGreenPin, brightness);
+      }
+      else
+      {
+        if (!manageGreenOnly)
+          digitalWrite(SAYellowPin, LOW);
+        digitalWrite(SAGreenPin, LOW);
+      }
+      infoCanMsg= receivedCanMsg;
     }
+  }
+
+}
+
+
