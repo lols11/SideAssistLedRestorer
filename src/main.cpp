a77f1818fe2468960f342e376e31e5cd8b5017a0
lols11 <lols11@spoko.pl>
2025-01-11 14:00:42 +0100

    Update main.cpp
    CAN message fix (added checksum)
    DPF LED blink fix
    MCP2515 lib change
    Watchdog

--- src/main.cpp
+++ src/main.cpp
@@ -1,148 +1,228 @@
-//Based on https://www.drive2.com/b/601314663967246021/
+/*
+    VW SideFix-DPFNotify v. 1.0
+
+    Copyright (c) 2025 by lols11
+    Contact: <contact@matbogucki.pl>
+    GitHub: https://github.com/lols11
+
+    Licensed under the MIT License.
+    See LICENSE and NOTICE files for details.
+*/
+
+/*
+    This project restores the functionality of the Side Assist LED
+    in the instrument cluster, resolving issues caused by a software update.
+    It also adds a notification feature for ongoing DPF regeneration by
+    blinking the DPF LED.
+
+    CAUTION: This software is intended for OFF-ROAD USE ONLY.
+*/
+
 #include <spi.h>
-#include <mcp2515.h>
+#include <mcp_can.h>
+#include <avr/wdt.h>
 
-#define SAYellowPin 6
-#define SAGreenPin 9
+//=======================================================  SETTINGS  =======================================================
 
-const static boolean manageLEDs = true;  //Should I manage the LED for side assist?
-const static boolean manageGreenOnly = true; //Should i've menage only green LED for SA?
+#define WATCHDOG_TIMEOUT WDTO_500MS
+#define MCP_FREQ MCP_8MHZ
+
+#define YELLOW_LED_PIN 6
+#define GREEN_LED_PIN 9
+
+const static boolean manageLEDs = true;      // Should I manage the LED for side assist?
+const static boolean manageGreenOnly = true; // Should i've menage only green LED for SA?
 const static boolean celebration = true;     // Should the SA LED celebration be turned on?
-const static boolean showDPFPurge = true; //Should the information about DPF cleaning be displayed?
-const unsigned long blinkIntervalTime = 250;    //Interval for blinking DPF light when purging
+const static boolean showDPFPurge = true;    // Should the information about DPF cleaning be displayed?
+const static boolean DPFPurgeBuzzer = true;  // Gong on start and end of DPF cleaning
+const unsigned long blinkIntervalTime = 250; // Interval for blinking DPF light when purging
 
-struct can_frame receivedCanMsg;
-struct can_frame infoCanMsg;
-struct can_frame drivetrainCanMsg;
+const unsigned int infoID = 0x03BA;       // Can ID for controlling communicats in instrument cluster (Side assist, buzzer)
+const unsigned int brightnessID = 0x0470; // Can ID for brightness of instrument cluster
+const unsigned int mMotor_5_ID = 0x0480;  // Can ID for drivetrain used for DPF purge info and turning DPF LED on
 
-static boolean dpfPurgeActualState = false;
-static boolean dpfPurgeLastState = false;
+//==========================================================================================================================
+
+long unsigned int rxId;
+unsigned char rxLen = 0;
+unsigned char rxCanMsg[8];
+unsigned char mMotor_5_msg[8];
+unsigned char info_msg[8];
+
+byte canStatus = CAN_FAILINIT;
+static bool blinkState = false;
+static bool dpfPurgeActualState = false;
+static bool dpfPurgeLastState = false;
 unsigned long blinkStartTime = 0;
 int brightness = 255;
-int canBrightness = 0x64; // Max value for instrument cluster brightness. 0x64 = 100(%) DEC
+int canBrightness = 0x64; // Max value for instrument cluster brightness
 
-MCP2515 mcp2515(10);
+MCP_CAN CAN0(10);
 
-canid_t infoID= 0x03BA;         // Can ID for controlling communicats in instrument cluster (Side assist, buzzer)
-canid_t brightnessID = 0x0470; // Can ID for brightness of instrument cluster
-canid_t drivetrainID = 0x0480; // Can ID for drivetrain used for DPF purge info and turning DPF LED on
+void reboot()
+{
+  wdt_enable(WDTO_15MS);
+  while (1)
+  {
+  }
+}
 
+uint8_t calc_xor_checksum(const uint8_t *d)
+{
+  uint8_t checksum = 0;
+  for (size_t i = 1; i < 8; i++)
+  {
+    checksum ^= d[i];
+  }
+  return checksum;
+}
 
 static void sendBeep()
 {
-  infoCanMsg.data[2] = infoCanMsg.data[2] | 0b00000001;
-  mcp2515.sendMessage(&infoCanMsg);
+  info_msg[2] = info_msg[2] | 0b00000001;
+  CAN0.sendMsgBuf(infoID, 0, 8, info_msg);
+  info_msg[2] &= ~0b00000001;
 }
 
 static void showDPFPurgeInfo()
 {
- 
-  if (!dpfPurgeActualState && !dpfPurgeLastState) //DPF Purge not active
+
+  if (!dpfPurgeActualState && !dpfPurgeLastState)
     return;
 
-  if (dpfPurgeActualState && !dpfPurgeLastState) //Start of DPF purge
+  if (dpfPurgeActualState && !dpfPurgeLastState)
   {
     blinkStartTime = millis();
-    sendBeep();
+    blinkState = false;
+    if (DPFPurgeBuzzer)
+      sendBeep();
     dpfPurgeLastState = true;
   }
 
-  if (!dpfPurgeActualState && dpfPurgeLastState) //End of DPF purge
+  if (!dpfPurgeActualState && dpfPurgeLastState)
   {
-    sendBeep();
+    if (DPFPurgeBuzzer)
+      sendBeep();
     dpfPurgeLastState = false;
     return;
   }
 
-  unsigned long actualTime = millis();
-  if (blinkStartTime + blinkIntervalTime >= actualTime) //DPF purge active (DPF LED blinking) 
+  if (dpfPurgeActualState)
   {
-    drivetrainCanMsg.data[5] = drivetrainCanMsg.data[5] | 0b00000010;
-  mcp2515.sendMessage(&drivetrainCanMsg);
-  }
-  else if (blinkStartTime + blinkIntervalTime * 2 >= actualTime)
-  blinkStartTime= millis();
+    unsigned long actualTime = millis();
 
-  
+    if (actualTime - blinkStartTime >= blinkIntervalTime)
+    {
+      blinkStartTime = actualTime;
+
+      blinkState = !blinkState;
+
+      if (blinkState)
+      {
+
+        mMotor_5_msg[5] |= 0b00000010;
+      }
+      else
+      {
+
+        mMotor_5_msg[5] &= ~0b00000010;
+      }
+      mMotor_5_msg[7] = calc_xor_checksum(mMotor_5_msg);
+      CAN0.sendMsgBuf(mMotor_5_ID, 0, 8, mMotor_5_msg);
+    }
+  }
 }
 
 void setup()
 {
+  wdt_enable(WATCHDOG_TIMEOUT);
   ADCSRA = 0;
-  pinMode(SAYellowPin, OUTPUT);
-  pinMode(SAGreenPin, OUTPUT);
-  digitalWrite(SAYellowPin, LOW);
-  digitalWrite(SAGreenPin, LOW);
+  pinMode(YELLOW_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  digitalWrite(YELLOW_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, LOW);
   SPI.begin();
 
-  mcp2515.reset();
-  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
-  mcp2515.setNormalMode();
+  canStatus = CAN0.begin(MCP_STD, CAN_500KBPS, MCP_FREQ);
+
+  if (canStatus != CAN_OK)
+    reboot();
+
+  canStatus = CAN0.setMode(MCP_NORMAL);
+
+  if (canStatus != CAN_OK)
+    reboot();
 
   // Celebration
   if (celebration == true)
   {
     if (!manageGreenOnly)
     {
-      digitalWrite(SAYellowPin, HIGH);
+      digitalWrite(YELLOW_LED_PIN, HIGH);
       delay(500);
     }
-    digitalWrite(SAGreenPin, HIGH);
+    digitalWrite(GREEN_LED_PIN, HIGH);
     delay(500);
     if (!manageGreenOnly)
     {
-      digitalWrite(SAYellowPin, LOW);
+      digitalWrite(YELLOW_LED_PIN, LOW);
       delay(250);
     }
-    digitalWrite(SAGreenPin, LOW);
+    digitalWrite(GREEN_LED_PIN, LOW);
   }
-
 }
 
 void loop()
 {
-
-  if (mcp2515.readMessage(&receivedCanMsg) == MCP2515::ERROR_OK)
+  if (CAN0.readMsgBuf(&rxId, &rxLen, rxCanMsg) == CAN_OK)
   {
-    if (showDPFPurge && receivedCanMsg.can_id == drivetrainID)
+
+    if (showDPFPurge && (rxId == mMotor_5_ID))
     {
-      if ((receivedCanMsg.data[0] & 0b100001) == 0b100001){
-        dpfPurgeActualState=true;
-        drivetrainCanMsg=receivedCanMsg;
-    }else
-        dpfPurgeActualState=false;
-    
-    showDPFPurgeInfo();
+
+      if ((rxCanMsg[0] & 0b100001) == 0b100001)
+      {
+        dpfPurgeActualState = true;
+        memcpy(mMotor_5_msg, rxCanMsg, sizeof(mMotor_5_msg));
+      }
+      else
+      {
+        dpfPurgeActualState = false;
+      }
+
+      showDPFPurgeInfo();
     }
 
-    if (manageLEDs && receivedCanMsg.can_id == brightnessID)
+    if (manageLEDs && (rxId == brightnessID))
     {
-    canBrightness = receivedCanMsg.data[4];
-    brightness = map(canBrightness, 0x1B, 0x64, 69, 255);
-    return;
+      canBrightness = rxCanMsg[4];
+      brightness = map(canBrightness, 0x1B, 0x64, 69, 255);
+      return;
     }
-    if (manageLEDs && receivedCanMsg.can_id == infoID)
+
+    if (manageLEDs && (rxId == infoID))
     {
-      if (receivedCanMsg.data[2] == 0x08 && !manageGreenOnly)
+      if ((rxCanMsg[2] == 0x08) && !manageGreenOnly)
       {
-        analogWrite(SAYellowPin, brightness);
-        digitalWrite(SAGreenPin, LOW);
+        analogWrite(YELLOW_LED_PIN, brightness);
+        digitalWrite(GREEN_LED_PIN, LOW);
       }
-      else if (receivedCanMsg.data[2] == 0x10)
+      else if (rxCanMsg[2] == 0x10)
       {
-        digitalWrite(SAYellowPin, LOW);
-        analogWrite(SAGreenPin, brightness);
+        digitalWrite(YELLOW_LED_PIN, LOW);
+        analogWrite(GREEN_LED_PIN, brightness);
       }
       else
       {
         if (!manageGreenOnly)
-          digitalWrite(SAYellowPin, LOW);
-        digitalWrite(SAGreenPin, LOW);
+        {
+          digitalWrite(YELLOW_LED_PIN, LOW);
+        }
+        digitalWrite(GREEN_LED_PIN, LOW);
       }
-      infoCanMsg= receivedCanMsg;
+
+      memcpy(info_msg, rxCanMsg, sizeof(info_msg));
     }
   }
-
+  wdt_reset();
 }
-
-
